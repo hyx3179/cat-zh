a56d757220d2f63909bdfb90f168c39d932a49e6
PeterCheney <809005839@qq.com>
2021-07-14 08:37:43 +0800

    换成CDN源

--- chs/chs.js
+++ chs/chs.js
@@ -17,7 +17,6 @@ var cnItems = {
     'autosaving...': '自动保存...',
     'Compress exported save file': '压缩导出的存档文件',
     'Nummon': '概览',
-    'Error: The email address is badly formatted.': '邮箱地址格式错误',
     'Error: The email address is already in use by another account.': '该邮箱地址已经被注册',
     '': '',
     '': '',
