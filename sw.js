739a0587ab9687bbac1aef216057784f96564d72
hyx3179 <hyx3179@gmail.com>
2022-04-22 22:18:00 +0800

    sw 为所有跨域资源添加 cors ，添加子目录

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const KGVision = '706'
+const Subfolder = '/cat-zh'
 const HOSTNAME_WHITELIST = [
 	self.location.hostname,
 	'cdn.jsdelivr.net',
@@ -8,12 +9,10 @@ const HOSTNAME_WHITELIST = [
 	'lf3-cdn-tos.bytecdntp.com',
 	//'p.qlogo.cn',
 	'www.googletagmanager.com',
-	'hyx3179.github.io',
-	'petercheney.gitee.io',
 ]
 const NO_CACHE_LIST = [
-	'/server.json',
-	'/build.version.json',
+	Subfolder + '/server.json',
+	Subfolder + '/build.version.json',
 	'/scientists/ks.version.json',
 ]
 const KS_LIST = [
@@ -21,9 +20,18 @@ const KS_LIST = [
 	'/scientists/ks.version.json',
 ]
 
+const getRequest = (req, cors) => {
+	var url = new URL(req.url)
+	if (url.hostname !== self.location.hostname) {
+		var newReq = new Request(url.href, { mode: 'cors' })
+		return newReq
+	}
+	return req
+}
+
 // 安装：加载 index 到 cache
 self.addEventListener('install', function (event) {
-	event.waitUntil(caches.open(KGVision).then(cache => cache.add('/')));
+	event.waitUntil(caches.open(KGVision).then(cache => cache.add(Subfolder + '/')));
 });
 
 // 激活：删除之前的 cache
@@ -50,42 +58,43 @@ self.addEventListener('fetch', event => {
 		} else {
 			var cacheName = KGVision
 		}
+		const req = getRequest(event.request)
 
-		if (NO_CACHE_LIST.indexOf(new URL(event.request.url).pathname) > -1) {
+		if (NO_CACHE_LIST.indexOf(new URL(req.url).pathname) > -1) {
 			// 无缓存文件优先请求网络
-			event.respondWith(fetch(event.request, { cache: 'no-store' })
+			event.respondWith(fetch(req, { cache: 'no-store' })
 				.then(resp => {
 					// 成功则拷贝一份
 					const respCopy = resp.clone()
 					// 在缓存中寻找之前的 cache，删除后提交新的 cache
-					Promise.all([respCopy, caches.match(event.request, { ignoreSearch: true })])
+					Promise.all([respCopy, caches.match(req, { ignoreSearch: true })])
 						.then(([respCopy, cacheold]) => {
 							Promise.all([respCopy, cacheold, caches.open(cacheName)])
 								.then(([response, cacheold, cache]) => {
 									if (cacheold !== undefined) {
 										cache.delete(cacheold.url, { ignoreSearch: true })
 									}
-									cache.put(event.request, response)
+									response.ok && cache.put(req, response)
 								})
 						})
 					return resp
 				})
 				.catch(_ => {
 					// 无网络时寻找并返回 cache
-					return caches.match(event.request, { ignoreSearch: true }).then(resp => resp)
+					return caches.match(req, { ignoreSearch: true }).then(resp => resp)
 				}))
 		} else {
 			// 其他情况优先请求 cache
-			event.respondWith(caches.match(event.request)
+			event.respondWith(caches.match(req)
 				.then(cached => {
 					if (cached !== undefined) {
 						return cached
 					} else {
 						// 不存在 cache，则请求网络并提交cache
-						const fetched = fetch(event.request, { cache: 'no-store' })
+						const fetched = fetch(req, { cache: 'no-store' })
 						const fetchedCopy = fetched.then(resp => resp.clone())
 						Promise.all([fetchedCopy, caches.open(cacheName)])
-							.then(([response, cache]) => cache.put(event.request, response))
+							.then(([response, cache]) => response.ok && cache.put(req, response))
 						return fetched
 					}
 				}))
