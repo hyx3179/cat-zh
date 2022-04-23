ec8c85286340e7104befba768552ad34a6ca0219
hyx3179 <hyx3179@gmail.com>
2022-04-24 03:22:49 +0800

    sw 请求多个网址 Netlify 添加重定向，与 GitHub 网址格式统一

--- generate-buildver.js
+++ generate-buildver.js
@@ -1,20 +1,24 @@
-var fs = require('fs');
-var FILE_NAME = "build.version.json";
+var fs = require("fs");
+var buildFile = "build.version.json"
+var publicFile = "./public/build.version.json"
 var GitHub = "sw-GitHub.js"
 var Netlify = "sw-Netlify.js"
 
-console.log('Incrementing build number...');
-fs.readFile(FILE_NAME, function (err, content) {
-    if (err) throw err;
-    var metadata = JSON.parse(content);
-    var str = 'const KGVision = \'' + metadata.buildRevision + '\'\nconst Subfolder = \'/cat-zh\'\nconst KSfolder = \'/scientists\'';
-    fs.writeFile(GitHub, str, function (err) {
-        if (err) throw err;
-        console.log(`sw-GitHub.js Current build number: ${metadata.buildRevision}`);
-    })
-    var str = 'const KGVision = \'' + metadata.buildRevision + '\'\nconst Subfolder = \'\'\nconst KSfolder = \'\'';
-    fs.writeFile(Netlify, str, function (err) {
-        if (err) throw err;
-        console.log(`sw-Netlify.js Current build number: ${metadata.buildRevision}`);
-    })
-});
+console.log("Incrementing build number...");
+var buildRevision = fs.readFileSync(buildFile)
+buildRevision = JSON.parse(buildRevision).buildRevision;
+
+var swRevision = fs.readFileSync(publicFile)
+swRevision = JSON.parse(swRevision).swRevision;
+swRevision = swRevision ? swRevision : 0
+
+var metadata = { buildRevision: buildRevision, swRevision: swRevision + 1 }
+fs.writeFile(publicFile, JSON.stringify(metadata), err => { if (err) throw err; })
+console.log(`Current build number: ${metadata.swRevision}`);
+
+var str = "const swRevision = " + metadata.swRevision
+	+ "\nconst GHCDN = 'https://cdn.jsdelivr.net/gh/hyx3179'"
+var strGitHub = str + "\nconst BACKUP = 'https://cat-zh-hyx3179.netlify.app'"
+fs.writeFile(GitHub, strGitHub, err => { if (err) throw err; })
+var strNetlify = str + "\nconst BACKUP = 'https://hyx3179.github.io'"
+fs.writeFile(Netlify, strNetlify, err => { if (err) throw err; })
--- sw.js
+++ sw.js
@@ -1,48 +1,77 @@
-// const KGVision = '706'
-// const Subfolder = '/cat-zh'
-// const KSfolder = '/scientists'
+const swRevision = 0
+const GHCDN = 'https://cdn.jsdelivr.net/gh/hyx3179'
+const BACKUP = 'https://cat-zh-hyx3179.netlify.app'
+// 这之前的行会在构建时删除 --------------------------
+const cacheStorage = 'cat-' + swRevision
 const HOSTNAME_BLACKLIST = [
 	'hm.baidu.com',
 	'p.qlogo.cn',
+	'q2.qlogo.cn',
 	'kittensgame.com',
 	'www.google-analytics.com',
 ]
 const NO_CACHE_LIST = [
-	Subfolder + '/server.json',
-	Subfolder + '/build.version.json',
-	KSfolder + '/ks.version.json',
+	'/cat-zh/server.json',
+	'/cat-zh/build.version.json',
+	'/scientists/ks.version.json',
 ]
 const KS_LIST = [
-	KSfolder + '/kitten-scientists.user.js',
-	KSfolder + '/ks.version.json',
+	'/scientists/kitten-scientists.user.js',
+	'/scientists/ks.version.json',
 ]
 
+// 获取请求
+// 跨域添加 cors
+// 可添加其他解析地址
 const getRequest = (req) => {
+	var reqList = [req]
 	var url = new URL(req.url)
 	if (url.hostname !== self.location.hostname) {
-		var newReq = new Request(url.href, { mode: 'cors' })
-		return newReq
+		let newReq = new Request(url.href, { mode: 'cors' })
+		reqList = [newReq]
+	} else {
+		if (GHCDN) {
+			let splitPosition = url.pathname.indexOf('/', 1)
+			let repository = url.pathname.slice(0, splitPosition)
+			let subFile = url.pathname.slice(splitPosition)
+			if (subFile !== '/') {
+				if (subFile.indexOf('.css') > -1) { subFile = subFile.slice(0, -4) + '.min.css' }
+				let href = GHCDN + repository + '@gh-pages' + subFile + url.search
+				let newReq = new Request(href, { mode: 'cors' })
+				reqList.push(newReq)
+			}
+		}
+		if (BACKUP) {
+			let href = BACKUP + url.pathname + url.search
+			let newReq = new Request(href, { mode: 'cors' })
+			reqList.push(newReq)
+		}
 	}
-	return req
+	return reqList
+}
+// 根据请求列表返回 fetchList
+const getFetch = (req) => {
+	var fetchList = []
+	for (let i in req) {
+		fetchList.push(fetch(req[i], { cache: 'no-store' }))
+	}
+	return fetchList
 }
 
 // 安装：加载 index 到 cache
-self.addEventListener('install', function (event) {
-	event.waitUntil(caches.open(KGVision).then(cache => cache.add(Subfolder + '/')));
+self.addEventListener('install', event => {
+	event.waitUntil(caches.open(cacheStorage).then(cache => cache.add('/cat-zh/')));
 });
 
 // 激活：删除之前的 cache
 self.addEventListener('activate', event => {
 	event.waitUntil(Promise.all([
 		self.clients.claim(),
-		caches.keys().then(function (cacheNames) {
-			return Promise.all(
-				cacheNames.filter(function (cacheName) {
-					return cacheName !== KGVision
-				}).map(function (cacheName) {
-					return caches.delete(cacheName);
-				}))
-		})]))
+		caches.keys().then(cacheNames =>
+			Promise.all(cacheNames.filter(cacheName => {
+				cacheName = cacheName.split('-')
+				return cacheName.length == 2 && cacheName[0] === 'cat' && cacheName[1] !== swRevision.toString()
+			}).map(cacheName => caches.delete(cacheName))))]))
 })
 
 // 请求处理
@@ -53,46 +82,44 @@ self.addEventListener('fetch', event => {
 		if (KS_LIST.indexOf(new URL(event.request.url).pathname) > -1) {
 			var cacheName = 'scientists'
 		} else {
-			var cacheName = KGVision
+			var cacheName = cacheStorage
 		}
 		const req = getRequest(event.request)
 
-		if (NO_CACHE_LIST.indexOf(new URL(req.url).pathname) > -1) {
+		if (NO_CACHE_LIST.indexOf(new URL(event.request.url).pathname) > -1) {
 			// 无缓存文件优先请求网络
-			event.respondWith(fetch(req, { cache: 'no-store' })
+			event.respondWith(Promise.race(getFetch(req))
 				.then(resp => {
 					// 成功则拷贝一份
 					const respCopy = resp.clone()
-					// 在缓存中寻找之前的 cache，删除后提交新的 cache
-					Promise.all([respCopy, caches.match(req, { ignoreSearch: true })])
-						.then(([respCopy, cacheold]) => {
-							Promise.all([respCopy, cacheold, caches.open(cacheName)])
-								.then(([response, cacheold, cache]) => {
-									if (cacheold !== undefined) {
-										cache.delete(cacheold.url, { ignoreSearch: true })
-									}
-									response.ok && cache.put(req, response)
-								})
+					// 删除缓存中的 cache 后提交新的 cache
+					Promise.all([respCopy, caches.open(cacheName)])
+						.then(([response, cache]) => {
+							cache.delete(req[0], { ignoreSearch: true })
+							cache.put(req[0], response)
 						})
 					return resp
 				})
 				.catch(_ => {
 					// 无网络时寻找并返回 cache
-					return caches.match(req, { ignoreSearch: true }).then(resp => resp)
+					return caches.match(req[0], { ignoreSearch: true }).then(resp => resp)
 				}))
 		} else {
 			// 其他情况优先请求 cache
-			event.respondWith(caches.match(req)
+			event.respondWith(caches.match(req[0])
 				.then(cached => {
 					if (cached !== undefined) {
 						return cached
 					} else {
-						// 不存在 cache，则请求网络并提交cache
-						const fetched = fetch(req, { cache: 'no-store' })
-						const fetchedCopy = fetched.then(resp => resp.clone())
-						Promise.all([fetchedCopy, caches.open(cacheName)])
-							.then(([response, cache]) => response.ok && cache.put(req, response))
-						return fetched
+						// 不存在 cache，则请求网络并提交 cache
+						// race() 返回最快的响应
+						return Promise.race(getFetch(req))
+							.then(resp => {
+								const respCopy = resp.clone()
+								Promise.all([respCopy, caches.open(cacheName)])
+									.then(([response, cache]) => cache.put(req[0], response))
+								return resp
+							})
 					}
 				}))
 		}
