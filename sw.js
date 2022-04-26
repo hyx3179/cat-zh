da2b77b99f6e2e5e9fbec6bcca118cdc688d7307
hyx3179 <hyx3179@gmail.com>
2022-04-26 23:31:37 +0800

    处理由多个 '/' 引起的问题 珂学家更新由版本号决定 sw 缓存空间仅由是否跨域决定

--- sw.js
+++ sw.js
@@ -15,21 +15,22 @@ const NO_CACHE_LIST = [
 	'/cat-zh/build.version.json',
 	'/scientists/ks.version.json',
 ]
-const KS_LIST = [
-	'/scientists/kitten-scientists.user.js',
-	'/scientists/ks.version.json',
-]
 
 // 获取请求
-// 跨域添加 cors
-// 可添加其他解析地址
-const getRequest = (req) => {
-	var reqList = [req]
-	var url = new URL(req.url)
-	if (url.hostname !== self.location.hostname) {
+const getRequest = (url) => {
+	url = new URL(url)
+	let reqList
+	if (url.hostname !== location.hostname) {
+		// 跨域添加 cors
 		let newReq = new Request(url.href, { mode: 'cors' })
 		reqList = [newReq]
 	} else {
+		// 重组 url 排除 '//+'
+		var pathname = '/' + url.pathname.split('/').filter(path => path.length > 0).join('/')
+		if (pathname.indexOf('/', 1) == -1) { pathname += '/' }
+		url = new URL(url.origin + pathname + url.search)
+		reqList = [new Request(url, { mode: 'no-cors' })]
+		// 添加其他解析地址
 		if (GHCDN) {
 			let splitPosition = url.pathname.indexOf('/', 1)
 			let repository = url.pathname.slice(0, splitPosition)
@@ -51,7 +52,7 @@ const getRequest = (req) => {
 }
 // 根据请求列表返回 fetchList
 const getFetch = (req) => {
-	var fetchList = []
+	let fetchList = []
 	for (let i in req) {
 		fetchList.push(fetch(req[i], { cache: 'no-store' }))
 	}
@@ -59,14 +60,14 @@ const getFetch = (req) => {
 }
 
 // 安装：加载 index 到 cache
-self.addEventListener('install', event => {
+addEventListener('install', event => {
 	event.waitUntil(caches.open(cacheStorage).then(cache => cache.add('/cat-zh/')));
 });
 
 // 激活：删除之前的 cache
-self.addEventListener('activate', event => {
+addEventListener('activate', event => {
 	event.waitUntil(Promise.all([
-		self.clients.claim(),
+		clients.claim(),
 		caches.keys().then(cacheNames =>
 			Promise.all(cacheNames.filter(cacheName => {
 				cacheName = cacheName.split('-')
@@ -75,18 +76,18 @@ self.addEventListener('activate', event => {
 })
 
 // 请求处理
-self.addEventListener('fetch', event => {
-	// 跳过一些 cross-origin （跨域）请求
-	if (HOSTNAME_BLACKLIST.indexOf(new URL(event.request.url).hostname) == -1) {
-		// 珂学家相关单开 cache 空间方便更新
-		if (KS_LIST.indexOf(new URL(event.request.url).pathname) > -1) {
-			var cacheName = 'scientists'
-		} else {
-			var cacheName = cacheStorage
+addEventListener('fetch', event => {
+	const url = new URL(event.request.url)
+	// 跳过无法正确处理 cors 的请求和不应缓存的请求
+	if (HOSTNAME_BLACKLIST.indexOf(url.hostname) == -1) {
+		let cacheName = cacheStorage
+		if (url.hostname !== location.hostname) {
+			cacheName = 'catStaticRes' // 跨域资源单独存放
 		}
-		const req = getRequest(event.request)
 
-		if (NO_CACHE_LIST.indexOf(new URL(event.request.url).pathname) > -1) {
+		const req = getRequest(url.href)
+
+		if (NO_CACHE_LIST.indexOf(new URL(req[0].url).pathname) > -1) {
 			// 无缓存文件优先请求网络
 			event.respondWith(Promise.race(getFetch(req))
 				.then(resp => {
@@ -111,13 +112,17 @@ self.addEventListener('fetch', event => {
 					if (cached !== undefined) {
 						return cached
 					} else {
-						// 不存在 cache，则请求网络并提交 cache
+						// 不存在 cache，则请求网络
+						// 删除以前可能存在的版本并提交 cache
 						// race() 返回最快的响应
 						return Promise.race(getFetch(req))
 							.then(resp => {
 								const respCopy = resp.clone()
 								Promise.all([respCopy, caches.open(cacheName)])
-									.then(([response, cache]) => cache.put(req[0], response))
+									.then(([response, cache]) => {
+										cache.delete(req[0], { ignoreSearch: true })
+										cache.put(req[0], response)
+									})
 								return resp
 							})
 					}
