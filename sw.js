5b0b739d80d3a1ca6a6cb92a9fe71252698c5adf
hyx3179 <hyx3179@gmail.com>
2022-04-27 18:54:38 +0800

    sw fetch 请求不成功延迟 5秒 返回    检查缓存的响应

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const swRevision = 0
-const GHCDN = 'https://cdn.jsdelivr.net/gh/hyx3179'
-const BACKUP = 'https://cat-zh-hyx3179.netlify.app'
+const GHCDN = ''//'https://cdn.jsdelivr.net/gh/hyx3179'
+const BACKUP = ''//'https://cat-zh-hyx3179.netlify.app'
 // 这之前的行会在构建时删除 --------------------------
 const cacheStorage = 'cat-' + swRevision
 const HOSTNAME_BLACKLIST = [
@@ -54,7 +54,15 @@ const getRequest = (url) => {
 const getFetch = (req) => {
 	let fetchList = []
 	for (let i in req) {
-		fetchList.push(fetch(req[i], { cache: 'no-store' }))
+		fetchList.push(fetch(req[i], { cache: 'no-store' })
+			.then(resp => {
+				if (!resp.ok) {
+					return new Promise(function (resolve) {
+						setTimeout(resolve, 5000, resp);
+					});
+				}
+				return resp
+			}))
 	}
 	return fetchList
 }
@@ -109,12 +117,12 @@ addEventListener('fetch', event => {
 			// 其他情况优先请求 cache
 			event.respondWith(caches.match(req[0])
 				.then(cached => {
-					if (cached !== undefined) {
+					if (cached !== undefined && cached.ok) {
 						return cached
 					} else {
-						// 不存在 cache，则请求网络
+						// 不存在 cache 或响应不在成功范围，则请求网络
 						// 删除以前可能存在的版本并提交 cache
-						// race() 返回最快的响应
+						// race() 返回最快的成功响应
 						return Promise.race(getFetch(req))
 							.then(resp => {
 								const respCopy = resp.clone()
